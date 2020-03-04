b2b361b1f5c1f67ff0daa3bb2fb9c1dae4c335d7
sevnCZ <wencz0321@gmail.com>
2020-03-04 11:36:26 +0800

    增加文件服务模块 增加restTemplate配置 增加Jackson转换配置

--- README.md
+++ README.md
@@ -7,12 +7,13 @@
 - Mybatis-plus
 - Postgres
 - Redisson
-- Fastdfs
 
 ## 模块说明
 ### web-common
 web模块的通用模块
 ### web-base
 web模块的基础模块，需要开发业务，直接引入该模块即可
+### web-file-service
+文件服务模块，提供文件上传，文件删除等服务，基于fastdfs
 ### web-demo
 web-base的使用示例
---
+++ web-demo/src/main/java/com/phlink/demo/controller/FileController.java
@@ -0,0 +1,65 @@
+package com.phlink.demo.controller;
+
+import com.github.tobato.fastdfs.domain.fdfs.StorePath;
+import com.phlink.core.common.enums.BaseResultInfo;
+import com.phlink.core.common.enums.CommonResultInfo;
+import com.phlink.core.common.exception.BizException;
+import com.phlink.file.service.FdfsStorageService;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Slf4j
+@RestController
+@RequestMapping("/file")
+@Validated
+public class FileController {
+
+    @Autowired
+    private FdfsStorageService fdfsStorageService;
+
+    @PostMapping("/single")
+    @ApiOperation(value = "上传一个文件", notes = "上传一个文件", httpMethod = "POST")
+    public String singleFile(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws BizException {
+        if (file.isEmpty()) {
+            throw new BizException(CommonResultInfo.BODY_NOT_MATCH, "上传的文件大小为空,请检查!!");
+        }
+        //获取文件名称、后缀名、大小
+        String fileName = file.getOriginalFilename();
+        if(StringUtils.isBlank(fileName)) {
+            throw new BizException(CommonResultInfo.BODY_NOT_MATCH, "文件名为空, 请检查!!");
+        }
+        String[] names = fileName.split("\\.");
+        String suffixName = names[names.length - 1];
+        long size = file.getSize();
+
+        String path = "";
+        try {
+            StorePath storePath = fdfsStorageService.simpleUpload(file.getInputStream(), size, suffixName);
+            path = storePath.getFullPath();
+        } catch (IOException e) {
+            log.error("上传的文件异常", e);
+            throw new BizException(CommonResultInfo.INTERNAL_SERVER_ERROR, "上传的文件异常!!");
+        }
+        return path;
+    }
+
+    @DeleteMapping("/single")
+    @ApiOperation(value = "删除一个已上传的文件", notes = "删除一个已上传的文件", httpMethod = "DELETE")
+    public Boolean deleteFile(@RequestParam("path") String path) {
+        try {
+            fdfsStorageService.deleteFile(path);
+        }catch (Exception e) {
+            throw new BizException(CommonResultInfo.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+        return true;
+    }
+}
