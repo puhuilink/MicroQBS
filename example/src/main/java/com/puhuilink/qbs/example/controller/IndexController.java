7c917752521dd3a682c8c32f3236c8d51131c57e
Sevncz <wencz0321@gmail.com>
2020-07-16 10:41:08 +0800

    update db sql

--- example/src/main/java/com/puhuilink/qbs/example/controller/IndexController.java
+++ example/src/main/java/com/puhuilink/qbs/example/controller/IndexController.java
@@ -2,13 +2,17 @@ package com.puhuilink.qbs.example.controller;
 
 import com.puhuilink.qbs.core.base.vo.Result;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("index")
 public class IndexController {
+    @Autowired
+    private RestTemplate restTemplate;
 
     @ApiOperation("Hello world")
     @GetMapping
