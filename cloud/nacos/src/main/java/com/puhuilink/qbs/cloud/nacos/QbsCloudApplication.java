1b53dd032ba098ffa3db61fe5ebf32d112a1ed71
Sevncz <wencz0321@gmail.com>
2020-07-29 17:10:12 +0800

    cloud update

--- cloud/nacos/src/main/java/com/puhuilink/qbs/cloud/nacos/QbsCloudApplication.java
+++ cloud/nacos/src/main/java/com/puhuilink/qbs/cloud/nacos/QbsCloudApplication.java
@@ -1,4 +1,4 @@
-package com.puhuilink.qbs.cloud;
+package com.puhuilink.qbs.cloud.nacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
