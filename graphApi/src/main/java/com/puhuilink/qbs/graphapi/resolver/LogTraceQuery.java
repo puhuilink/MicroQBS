adb247f2295c21977a406143c3171bfac54aae4e
Sevncz <wencz0321@gmail.com>
2020-08-19 17:20:14 +0800

    update

--- graphApi/src/main/java/com/puhuilink/qbs/graphapi/resolver/LogTraceQuery.java
+++ graphApi/src/main/java/com/puhuilink/qbs/graphapi/resolver/LogTraceQuery.java
@@ -1,8 +1,6 @@
-package com.puhuilink.qbs.graphapi.query;
+package com.puhuilink.qbs.graphapi.resolver;
 
-import com.puhuilink.qbs.core.logtrace.annotation.SystemLogTrace;
 import com.puhuilink.qbs.core.logtrace.entity.LogTrace;
-import com.puhuilink.qbs.core.logtrace.enums.LogType;
 import com.puhuilink.qbs.core.logtrace.service.LogTraceService;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +9,15 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-public class LogTraceQuery implements GraphQLQueryResolver{
+public class LogTraceQuery implements GraphQLQueryResolver {
     @Autowired
     public LogTraceService logTraceService;
 
-    @SystemLogTrace(description = "Query Graphql Api", type = LogType.QUERY)
-    public List<LogTrace> listLogtrace() {
+    public List<LogTrace> logTraces() {
         return logTraceService.list();
+    }
+
+    public LogTrace logTrace(String id) {
+        return logTraceService.getById(id);
     }
 }
